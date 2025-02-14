0219d9d648f7d073b8305cb869d3ae4687d5176d
Assia TAHTAOUI <129065960+assiatahtaoui@users.noreply.github.com>
2025-02-14 12:43:59 +0100

    Add files via upload

---
+++ SystemBank/src/Bank/Management/sys/Login.java
@@ -0,0 +1,44 @@
+package Bank.Management.sys;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Login extends JFrame {
+    Login(){
+        super();
+        ImageIcon i1=new ImageIcon(ClassLoader.getSystemResource("icon/bank.png"));
+        Image i2=i1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon i3=new ImageIcon(i2);
+        JLabel image=new JLabel(i3);
+        image.setBounds(350,10,100,100);
+        add(image);
+        ImageIcon ii1=new ImageIcon(ClassLoader.getSystemResource("icon/card.png"));
+        Image ii2=ii1.getImage().getScaledInstance(100,100,Image.SCALE_DEFAULT);
+        ImageIcon ii3=new ImageIcon(ii2);
+        JLabel iimage=new JLabel(ii3);
+        iimage.setBounds(650,350,100,100);
+        add(iimage);
+        ImageIcon iii1=new ImageIcon(ClassLoader.getSystemResource("icon/backbg.png"));
+        Image iii2=iii1.getImage().getScaledInstance(850,480,Image.SCALE_DEFAULT);
+        ImageIcon iii3=new ImageIcon(iii2);
+        JLabel iiimage=new JLabel(iii3);
+        iiimage.setBounds(0,0,850,480);
+        add(iiimage);
+
+
+
+        setLayout(null);
+        setSize(850,480);
+        setLocation(450,200);
+        setVisible(true);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        new Login();
+
+    }
+
+}
